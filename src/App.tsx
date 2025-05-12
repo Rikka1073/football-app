03ce66c603da582b825147393d4382ef1efa183d
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-05-12 20:48:18 +0900

    useSWRインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import { Button } from "@yamada-ui/react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  // axios
+  //   .get("/api/v4/competitions/PL/teams", {
+  //     headers: {
+  //       "X-Auth-Token": import.meta.env.VITE_API_KEY,
+  //     },
+  //   })
+  //   .then((res) => {
+  //     console.log(res.data);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
   return (
     <>
       <Button>ボタン</Button>
